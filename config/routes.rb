e0c9d1abcd26a0ccf7c037547d2eb8af6893267e
test <test@example.com>
2017-07-18 03:42:51 -0700

    add login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+   root 'static_pages#home'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -13,6 +15,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
-  resources :users
+   resources :users
 end
